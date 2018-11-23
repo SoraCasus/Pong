d6fea53a6e280913f0f7997aee8c73e8a213229e
Sora <joshua90123@gmail.com>
2018-11-23 09:36:51 -0700

    Update

--- Compile.bat
+++ Compile.bat
@@ -1,3 +1,4 @@
 C:\Dev\asm\AssemblyEnvironment\Masm615\ML.exe -I"C:\Dev\asm\AssemblyEnvironment\Masm615\INCLUDE" -Zi -c -Fl -coff "C:\Dev\asm\AssemblyEnvironment\src\Pong\Pong.asm"
 C:\Dev\asm\AssemblyEnvironment\Masm615\LINK32.exe /LIBPATH:"C:\Dev\asm\AssemblyEnvironment\Masm615\LIB" "C:\Dev\asm\AssemblyEnvironment\src/Pong\Pong.obj" irvine32.lib kernel32.lib /SUBSYSTEM:CONSOLE /DEBUG
 pause
+Pong.exe
--- Pong.asm
+++ Pong.asm
@@ -7,9 +7,13 @@ INCLUDE VirtualKeys.inc
     blankLines      BYTE 80 dup(219), 0
     deltaX          BYTE 1
     deltaY          BYTE 1
+    ballX           BYTE 40
+    ballY           BYTE 10
+    prevBallX       BYTE 40
+    prevBallY       BYTE 10
     ballCount       BYTE 5
     playerPos       BYTE 10
-    __PLAYER_MAX__  = 74
+    __PLAYER_MAX__  = 18
 .code
 
 main PROC
@@ -44,24 +48,99 @@ loop        border
 main ENDP
 
 GameLoop PROC
+top:
     call        Input
     call        DrawPlayer
-
-    pushad
-    mov         dl, 10
-    mov         dh, 10
-    call        Gotoxy
-    movzx       eax, playerPos
-    call        WriteInt
-    popad
+    call        UpdateBall
 
     mov         eax, 100
     call        Delay
+    jmp         top    
 GameLoop ENDP
+
+UpdateBall PROC
+    push        eax
+    push        edx
+
+    mov         dl, ballX       ; Clear the ball from the previous location
+    mov         dh, ballY
+    call        Gotoxy
+    
+    mov         al, ' '
+    call        WriteChar
+
+    mov         al, ballY       ; Bottom wall collision
+    cmp         al, 22
+    jb          notBottomWall
+    neg         deltaY
+notBottomWall:
+
+    mov         al, ballY       ; Top wall collision
+    cmp         al, 1
+    ja          notTopWall
+    neg         deltaY
+notTopWall:
+
+    mov         al, ballX       ; Paddle collision
+    cmp         al, 74
+    jbe          notPaddle
+
+    mov         al, ballY
+    cmp         al, (playerPos + 5)
+    jae          notPaddle
+    cmp         al, playerPos
+    jbe         notPaddle
+
+    neg         deltaX
+
+notPaddle:
+    push        ecx
+    mov         ecx, 5
+topLoop:
+    mov         dl, 74
+    push        ecx
+    add         cl, playerPos
+    mov         dh, cl
+    pop         ecx
+    call        Gotoxy
+    mov         al, 'B'
+    call        WriteChar
+    loop        topLoop
+    pop         ecx
+
+    mov         al, ballX       ; Update the ball's X position
+    add         al, deltaX
+    mov         ballX, al
+
+    mov         al, ballY       ; Update the ball's Y position
+    add         al, deltaY
+    mov         ballY, al
+
+    mov         dl, ballX       ; Draw the ball in the new position
+    mov         dh, ballY
+    call        Gotoxy      
+    mov         al, '@'
+    call        WriteChar
+
+    pop         edx
+    pop         eax
+    ret
+UpdateBall ENDP
 
 DrawPlayer PROC
 
     pushad
+
+    mov         dl,75           ; Clear the previous Paddle pixels
+    mov         dh, 1
+    call        Gotoxy
+    mov         ecx, (__PLAYER_MAX__ + 4)
+    mov         al, ' '
+clearLoop:
+    call        WriteChar
+    inc         dh
+    call        Gotoxy
+    loop        clearLoop
 
     mov         dl, 75
     mov         dh, playerPos
@@ -93,13 +172,13 @@ Input PROC
     ; Todo(Sora): Test the max player position
     mov         edx, 0
     call        ReadKey
-    and         al, 0DFh
-    cmp         al, 'W'     ; Is up key pressed
+    ;and         al, 0DFh
+    cmp         al, 077h     ; Is up key pressed
     jne         notUp
     call        MoveUp
 notUp:
-    and         al, 0DFh
-    cmp         al, 'S'     ; Is down key pressed
+    ;and         al, 0DFh
+    cmp         al, 073h     ; Is down key pressed
     jne         notDown
     call        MoveDown
 notDown:
@@ -112,19 +191,11 @@ MoveUp PROC
 
     push        eax
 
-    push        edx
-    mov         dl, 11
-    mov         dh, 11
-    call        Gotoxy
-    mov         edx, offset upPressedS
-    call        WriteString
-    pop         edx
-
     movzx       eax, playerPos    
-    cmp         eax, __PLAYER_MAX__
-    jae         tooBig
+    cmp         eax, 1
+    jbe          tooBig
 
-    inc         playerPos
+    dec         playerPos
 
 tooBig:
     pop         eax
@@ -136,20 +207,11 @@ MoveDown PROC
 
     push        eax
 
-
-    push        edx
-    mov         dl, 11
-    mov         dh, 11
-    call        Gotoxy
-    mov         edx, offset downPressedS
-    call        WriteString
-    pop         edx
-
     movzx       eax, playerPos    
-    cmp         eax, 0
-    jz         tooSmall
+    cmp         eax, __PLAYER_MAX__
+    jae         tooSmall
 
-    dec         playerPos
+    inc         playerPos
     
 tooSmall:
     pop         eax
