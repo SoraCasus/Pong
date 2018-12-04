fd9094af0c50c60b1ef4746299b5e0ac20d73d6e
Sora <joshua90123@gmail.com>
2018-12-03 22:35:17 -0700

    Final upload

--- Pong.asm
+++ Pong.asm
@@ -2,22 +2,28 @@ INCLUDE Irvine32.inc
 INCLUDE VirtualKeys.inc
 
 .data
-    upPressedS      BYTE "Up     ", 0
-    downPressedS    BYTE "Down   ", 0
-    blankLines      BYTE 80 dup(219), 0
-    deltaX          BYTE 1
-    deltaY          BYTE 1
-    ballX           BYTE 40
-    ballY           BYTE 10
-    prevBallX       BYTE 40
-    prevBallY       BYTE 10
-    ballCount       BYTE 5
-    playerPos       BYTE 10
+	directions			SBYTE -1, 1
+    upPressedS      	BYTE "Up     ", 0
+    downPressedS    	BYTE "Down   ", 0
+	livesS				BYTE "Lives: ", 0
+	gameOverS			BYTE "Game Over!", 0
+	scoreS				BYTE "Score: ", 0
+    blankLines      		BYTE 80 dup(219), 0
+    deltaX          		BYTE 1
+    deltaY          		BYTE 1
+    ballX           		BYTE 40
+    ballY           		BYTE 10
+    prevBallX       		BYTE 40
+    prevBallY       		BYTE 10
+    ballCount       		BYTE 5
+    playerPos       	BYTE 10
+	lives					BYTE 5
+	score					BYTE 0
     __PLAYER_MAX__  = 18
 .code
 
 main PROC
-    mov         eax, black + (blue * 16)
+    mov         eax, white + (blue * 16)
     call        SetTextColor
     call        ClrScr                    ; Initialization Code
     mov         edx, 0
@@ -38,6 +44,28 @@ loop        border
 
     mov         edx, 4
     call        Gotoxy
+	
+	
+	call		Randomize
+	mov		eax, 2
+	call		RandomRange
+	cmp		eax, 1
+	jne		positive
+	mov		deltaX, 1
+	jmp		aroundA
+positive:
+	mov		deltaX, -1
+aroundA:
+	
+	mov		eax, 2
+	call		RandomRange
+	cmp		eax, 1
+	jne 		positive1
+	mov		deltaY, 1
+	jmp 		aroundB
+positive1:
+	mov		deltaY, -1
+aroundB:
 
     call        GameLoop
 
@@ -64,49 +92,55 @@ UpdateBall PROC
 
     mov         dl, ballX       ; Clear the ball from the previous location
     mov         dh, ballY
-    call        Gotoxy
+    call        	Gotoxy
     
     mov         al, ' '
-    call        WriteChar
+    call        	WriteChar
 
     mov         al, ballY       ; Bottom wall collision
     cmp         al, 22
-    jb          notBottomWall
+    jb          	notBottomWall
     neg         deltaY
+	jmp			notYCollide
 notBottomWall:
 
     mov         al, ballY       ; Top wall collision
     cmp         al, 1
-    ja          notTopWall
+    ja          	notTopWall
     neg         deltaY
+	
 notTopWall:
-
+notYCollide:
     mov         al, ballX       ; Paddle collision
-    cmp         al, 74
-    jbe          notPaddle
+    cmp         al, 73
+    jbe          notRightWall
 
     mov         al, ballY
-    cmp         al, (playerPos + 5)
+	mov		   ah, playerPos
+	add		   ah, 5
+    cmp         al, ah
     jae          notPaddle
-    cmp         al, playerPos
-    jbe         notPaddle
+    cmp        al, playerPos
+    jbe         	notPaddle
 
     neg         deltaX
+	inc		  score
 
+	jmp			notRightWall
 notPaddle:
-    push        ecx
-    mov         ecx, 5
-topLoop:
-    mov         dl, 74
-    push        ecx
-    add         cl, playerPos
-    mov         dh, cl
-    pop         ecx
-    call        Gotoxy
-    mov         al, 'B'
-    call        WriteChar
-    loop        topLoop
-    pop         ecx
+	
+	dec			lives
+	
+	call			ResetRound
+	
+notRightWall:
+
+	mov			al, ballX
+	cmp			ballX, 1
+	ja				notLeftWall
+	neg			deltaX
+
+notLeftWall:
 
     mov         al, ballX       ; Update the ball's X position
     add         al, deltaX
@@ -126,6 +160,66 @@ topLoop:
     pop         eax
     ret
 UpdateBall ENDP
+
+ResetRound PROC
+	
+	push 	eax
+	push		edx
+	
+	mov		dl, 10
+	mov		dh, 5
+	call		Gotoxy
+	mov		edx, offset livesS
+	call		WriteString
+	movzx	eax, lives
+	call		WriteDec
+	
+	mov		ballX, 40
+    mov		ballY, 10
+	
+	call		Randomize
+	mov		eax, 2
+	call		RandomRange
+	cmp		eax, 1
+	jne		positive
+	mov		deltaX, 1
+	jmp		aroundA
+
+positive:
+	mov		deltaX, -1
+aroundA:
+	
+	mov		eax, 2
+	call		RandomRange
+	cmp		eax, 1
+	jne 		positive1
+	mov		deltaY, 1
+	jmp 		aroundB
+positive1:
+	mov		deltaY, -1
+aroundB:
+
+	cmp		lives, 0
+	ja 		continue
+	
+	mov 		dl, 15
+	mov		dh, 20
+	call		Gotoxy
+	
+	mov		edx, offset gameOverS
+	call		WriteString
+	
+	call		WaitMsg
+	
+	exit
+continue:
+
+	pop		edx
+	pop		eax
+
+	ret
+
+ResetRound ENDP
 
 DrawPlayer PROC
 
@@ -160,6 +254,22 @@ clearLoop:
     inc         dh
     call        Gotoxy
     call        WriteChar
+	
+	mov		dl, 10
+	mov		dh, 5
+	call		Gotoxy
+	mov		edx, offset livesS
+	call		WriteString
+	movzx	eax, lives
+	call		WriteDec
+	
+	mov		dl, 10
+	mov		dh, 6
+	call		Gotoxy
+	mov		edx, offset scoreS
+	call		WriteString
+	movzx	eax, score
+	call		WriteDec
 
     popad
 
@@ -196,6 +306,7 @@ MoveUp PROC
     jbe          tooBig
 
     dec         playerPos
+	dec			playerPos
 
 tooBig:
     pop         eax
@@ -208,10 +319,11 @@ MoveDown PROC
     push        eax
 
     movzx       eax, playerPos    
-    cmp         eax, __PLAYER_MAX__
-    jae         tooSmall
+    cmp          eax, __PLAYER_MAX__
+    jae            tooSmall
 
     inc         playerPos
+	inc		 playerPos
     
 tooSmall:
     pop         eax
